707d893e489f7377d97098bb06205ad922ec7532
Miguel Pereira da Silva <miguel.silva@avanade.com>
2022-06-09 18:59:40 -0300

    Leitura de arquivo delimitado

--- src/main/java/br/com/estudandoemcasa/spring/batch/JobSpringBatchApplication.java
+++ src/main/java/br/com/estudandoemcasa/spring/batch/JobSpringBatchApplication.java
@@ -3,11 +3,18 @@ package br.com.estudandoemcasa.spring.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class JobSpringBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobSpringBatchApplication.class, args);
+
+
 	}
 
 }
--- src/main/java/br/com/estudandoemcasa/spring/batch/reader/LeituraArquivoLarguraDelimitadoReaderConfig.java
+++ src/main/java/br/com/estudandoemcasa/spring/batch/reader/LeituraArquivoLarguraDelimitadoReaderConfig.java
@@ -1,25 +1,27 @@
 package br.com.estudandoemcasa.spring.batch.reader;
 
 import br.com.estudandoemcasa.spring.batch.model.Cliente;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.Range;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.PathResource;
 
-//@Configuration
-public class LeituraArquivoLarguraFixaReaderConfig {
+@Configuration
+public class LeituraArquivoLarguraDelimitadoReaderConfig {
 
-//    @StepScope
-//    @Bean
-    public FlatFileItemReader<Cliente> leituraArquivoLarguraFixaReader(
+    @StepScope
+    @Bean
+    public FlatFileItemReader<Cliente> leituraArquivoLarguraDelimitadoReader(
             @Value("#{jobParameters['arquivoClientes']}") String arquivoClientes){
 
         return new FlatFileItemReaderBuilder<Cliente>()
-                .name("leituraArquivoLarguraFixaReader")
+                .name("leituraArquivoLarguraDelimitadoReader")
                 .resource(new PathResource(arquivoClientes))
-                .fixedLength()
-                .columns(new Range[] {new Range(1,10), new Range(11,20), new Range(21,23), new Range(24,43)})
+                .delimited()
                 .names(new String[] {"nome", "sobrenome", "idade", "email"})
                 .targetType(Cliente.class)
                 .build();
